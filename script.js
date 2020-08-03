464a45f7b95d50ae2c69043d2f8ae98a5c9b604d
MariaBiryukova <moon.cheza@bk.ru>
2020-08-03 14:59:44 +0300

    сделал анимацию светофора

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 'use strict';
 
 // window.addEventListener('click', setInterval(flashLights, 1000));
-function redLights () {
-	let lightStrips = document.querySelectorAll('.light-strip');	
-	lightStrips.forEach ((item) => {
-		let lights = item.children;	
-		function red (i) {
-			if (i > 1) {					
-				lights[i].classList.add('red');					
-			}
-		}
-		for (let i = 0; i < lights.length; i++) {
-			setTimeout(red, 1000, i);
+// function redLights () {
+// 	let lightStrips = document.querySelectorAll('.light-strip');	
+// 	lightStrips.forEach ((item) => {
+// 		let lights = item.children;	
+// 		function red (i) {
+// 			if (i > 1) {					
+// 				lights[i].classList.add('red');					
+// 			}
+// 		}
+// 		for (let i = 0; i < lights.length; i++) {
+// 			setTimeout(red, 1000, i);
 			
 				
-			}		
-		}
-	);
+// 			}		
+// 		}
+// 	);
+// }
+// redLights ();
+
+let i = 0;
+setInterval(flashLights, 1000);
+
+function flashLights () {
+	let lightStrips = document.querySelectorAll('.light-strip'),
+		lights = lightStrips[i].children;
+		lights[2].classList.add('red');
+		lights[3].classList.add('red');
+		i++;
 }
-redLights ();
+
+
 
 // function addRed () {
 // 	let lightStrips = document.querySelectorAll('.light-strip');
