a2a0a576e45a06a08f1d6368a14975e968d19b02
VBiryukov <versus1711@gmail.com>
2020-08-05 02:03:37 +0300

    Таймер готов

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 'use strict';
 
 window.addEventListener('click', countDown);
+let timerID,
+	j = 0,
+	i = 0,
+	start,
+	time = document.querySelector('.time');
+
 
 function countDown () {
-	setTimeout(flashLights, 1000);
+	time.innerHTML = '00.000';
+	if (j % 2 === 0) {
+		timerID = setInterval(flashLights, 1000, timerID);
+	}	
+	else {
+		finish();
+	}
+	j++;
 }
 
-let i = 0;
-let start;
 function flashLights () {
 	let lightStrips = document.querySelectorAll('.light-strip'),
 		lights = lightStrips[i].children;
 		lights[2].classList.add('red');
 		lights[3].classList.add('red');
 		i++;
-		if (i < lightStrips.length) {
-			setTimeout(flashLights, 1000);
-		}
-		else {
-			window.removeEventListener('click', countDown);
-			setTimeout(lightsOff, 1000);
+		if (i >= lightStrips.length) {
+			let timeOut = randomTime();
+			setTimeout(lightsOff, timeOut);
+			i = 0;
 		}
 }
 
 function lightsOff () {
+	clearTimeout(timerID);
 	let lights = document.querySelectorAll('.light');
 	lights.forEach ((item) => {
 	item.classList.remove('red');
 	});
 	start = new Date();
-	console.log(start);
+	i = 0;
 }
 
+function finish () {	
+	if (start == undefined) {
+		lightsOff ();
+		time.innerHTML = 'Фальстарт';
+	}
+	else {
+		let finishTime = new Date();
+		time.innerHTML = ((finishTime - start) / 1000);
+	}	
+	start = undefined;
+}
+
+function randomTime () {
+	return Math.trunc((Math.random() * (2 - 0.2) + 0.2) * 1000) ;
+}
